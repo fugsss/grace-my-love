e8d98d68a031ac78adb9535b5fc34f0f02a0e88d
fugsss <tanggggui@gmail.com>
2026-02-07 19:25:18 +0800

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://open.spotify.com/track/6sGIMrtIzQjdzNndVxe397?si=f6c4654db4824828", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/deoi0x3ql/video/upload/v1770463406/Olivia_Dean_-_So_Easy_To_Fall_In_Love_Lyric_Video_48kbps_kcbwzb.mp4", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
